37e485caeab3dabb7e646d4c2240e81d348afb39
AmauriLima <amauri.plimaj@gmail.com>
2022-05-31 17:31:01 -0300

    css

--- src/components/PageHeader/styles.ts
+++ src/components/PageHeader/styles.ts
@@ -3,5 +3,5 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   justify-content: center;
   align-items: center;
-  margin: 32px 0;
+  margin: 32px 0 16px;
 `;
